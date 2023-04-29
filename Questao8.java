70d7411497e9fbddd209c6893070edb8d79b3fc8
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-04-29 14:39:27 -0300

    Update Questao8.java

--- Questao8.java
+++ Questao8.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 public class Questao8 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     System.out.printf("Total de veículos:");
-     int veiculos = sc.nextInt();
+     int veiculos = scanner.nextInt();
     System.out.printf("Total de rodas(Digite um número par):");
-     int rodas = sc.nextInt();
-    
-    //Aqui coloquei uma linha divisória//
-    System.out.println("-----------------------------");
+     int rodas = scanner.nextInt();
     
      int motos = ((4 * veiculos) - rodas)/2;
      int carros = (veiculos - motos);
 
     System.out.printf("No estacionamento há " + (carros) + " carros e " + (motos) + " motos");
 
-    sc.close();
+    scanner.close();
   }
 }
