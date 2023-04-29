cc995d0086051440cebd6b7e05dee23edd80f838
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-04-29 14:08:33 -0300

    Update Questao3.java

--- Questao3.java
+++ Questao3.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 public class Questao3 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Escreva uma temperatura em graus Fahrenhenit(F°):");
-      int f = sc.nextInt();
+      int Fahrenhenit = scanner.nextInt();
     
-    //Aqui coloquei uma linha divisória//
-    System.out.println("----------------------------------");
-    System.out.printf("Essa temperatura em graus centígrados é: " + (f - 32) / 1.8 + "°C");
+    System.out.printf("Essa temperatura em graus centígrados é: " + (Fahrenhenit - 32) / 1.8 + "°C");
 
-    sc.close();
+    scanner.close();
   }
 }
