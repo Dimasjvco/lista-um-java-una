e38dfdfa8c05e57af38d5846a413266c441972dd
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-04-29 14:10:14 -0300

    Update Questao4.java

--- Questao4.java
+++ Questao4.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 public class Questao4 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Digite o valor do capital a ser aplicado:");
-     double C = sc.nextDouble();
+     double C = scanner.nextDouble();
     System.out.println("Digite o prazo de aplicação em meses:");
-     double n = sc.nextDouble();
+     double n = scanner.nextDouble();
     System.out.println("Digite a taxa de juros mensal(20% = 20):");
-     double i = sc.nextDouble();
+     double i = scanner.nextDouble();
 
-    //Aqui eu parti a operação e finalizei no final do programa//
     double I = (i / 100 + 1);
     double m = Math.pow(I,n);
     double M = (C * m);
 
-    //Aqui coloquei uma divisória//
-    System.out.println("-------------------------------------");
     System.out.printf("O montante a ser recebido é de: R$ %.2f", M);
 
     sc.close();
