c1a867bcb179bdea53f2c9931870c3ff62a9e679
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-04-29 14:24:30 -0300

    Update Questao5.java

--- Questao5.java
+++ Questao5.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 public class Questao5 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         
         System.out.println("Número de horas que o funcionário trabalhou no mês:");
-         double horas = sc.nextDouble();
+         double horas = scanner.nextDouble();
         System.out.println("Valor recebido por hora de trabalho:");
-         double valorhora = sc.nextDouble();
+         double valorhora = scanner.nextDouble();
         System.out.println("Valor do salário família:");
-         double family = sc.nextDouble();
+         double familia = scannernextDouble();
         System.out.println("Número de filhos com idade menor que 14 anos:");
-         double filhos = sc.nextDouble();
+         double filhos = scanner.nextDouble();
 
-        //Aqui coloquei uma linha divisória//
-        System.out.println("------------------------------------------------");
 
-         double valorbruto = (horas * valorhora) + filhos * family;
+
+         double valorbruto = (horas * valorhora) + filhos * familia;
         System.out.printf("O salário bruto a ser recebido por um funcionário em um mês é de: R$%.2f", valorbruto);
 
-     sc.close();
+     scanner.close();
     }
 }
