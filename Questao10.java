9b3e3b4b714f98f5b6b6cd53e372740c68391d59
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-04-29 14:46:47 -0300

    Update Questao10.java

--- Questao10.java
+++ Questao10.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 public class Questao10 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     double ma, mh, mg;
   
     System.out.printf("Digite o 1° número:");
-     double n1 = sc.nextInt();
+     double n1 = scanner.nextInt();
     System.out.printf("Digite o 2° número:");
-     double n2 = sc.nextInt();
+     double n2 = scanner.nextInt();
     System.out.printf("Digite o 3° número:");
-     double n3 = sc.nextInt();
-
-    //Aqui coloquei uma linha divisória//
-    System.out.println("--------------------------------");
+     double n3 = scanner.nextInt();
 
      ma = ((n1 + n2 + n3) / 3);
      mh = (3 / (1/n1 + 1/n2 + 1/n3));
@@ -22,6 +19,6 @@ public class Questao10 {
     System.out.printf("A média harmônica desses valores é: %.2f", mh);
     System.out.printf("A média geométrica desses valores é: %.2f", mg);
 
-    sc.close();
+    scanner.close();
   }
 }
