bfceefa12c6833909156668a0d333962c74f766d
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-04-29 14:36:18 -0300

    Update Questao7.java

--- Questao7.java
+++ Questao7.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 public class Questao7 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Salário fixo de um vendedor:");
-     double salariofixo = sc.nextDouble();
+     double salariofixo = scanner.nextDouble();
     System.out.println("Valor total das vendas no mês");
-     double vendasmes = sc.nextDouble();
+     double vendasmes = scanner.nextDouble();
     System.out.println("Percentual sobre o total das vendas mensais(20% = 20):");
      double porcentagemtotalvendas = sc.nextDouble();
 
-    //Aqui coloquei uma linha divisória//
-    System.out.println("-------------------------------------------");
-
     System.out.printf("O seu salário total é de: R$" + (salariofixo + vendasmes * porcentagemtotalvendas / 100));
     
-    sc.close();
+    scanner.close();
   }
 }
